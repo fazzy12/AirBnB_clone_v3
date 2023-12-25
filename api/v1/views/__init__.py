280bd03d1200fa0f0bc043409af8d4f0572d4276
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-25 17:40:16 +0100

    Implement initial API structure and status endpoint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains the blueprint for the API
-"""
+""" Blueprint for API v1 """
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+# This is intentionally left empty for wildcard imports.
+# Wildcard imports are discouraged by PEP8 but are okay for this use-case.
