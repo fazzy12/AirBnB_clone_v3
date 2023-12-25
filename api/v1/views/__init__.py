fefe789a08574496e8353bd21bfd78ea4906c533
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-25 19:41:07 +0100

    Implement initial API structure and status endpoint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-"""
-Contains the blueprint for the API
-"""
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import and register your routes
+from api.v1.views.index import *
