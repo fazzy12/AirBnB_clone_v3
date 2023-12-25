4148cb327e218cb3a8e0f65a2670db09b69aa4e2
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-25 20:03:34 +0100

    Implement initial API structure and status endpoint

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,19 @@
 
 import os
 from flask import Flask, jsonify
-# from flask_cors import CORS
+from flask_cors import CORS
 
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
