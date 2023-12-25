6cf841674da76137697678d53a1249045dd2121d
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-25 20:44:02 +0100

    Add endpoint for retrieving object type statistics

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,18 @@ from models import storage
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieve the numbers of each object type."""
+    
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    
+    return jsonify(stats)
