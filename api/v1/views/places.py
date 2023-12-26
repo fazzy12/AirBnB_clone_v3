80d044a9a7061def70e8fe7328585ce7c8eb7604
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-26 12:39:33 +0100

    Implement RESTful API actions for Place objects

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """Places view module"""
 
+from flask import abort, jsonify, request
 from api.v1.views import app_views
-from flask import jsonify, abort, request
-from models import storage, Place, City, User
+from models import storage
+from models.city import City
+from models.place import Place
+from models.user import User
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>/places',
+                 methods=['GET'], strict_slashes=False)
 def get_places_by_city(city_id):
     """Retrieve all Place objects for a City"""
     city = storage.get(City, city_id)
@@ -21,7 +26,8 @@ def get_place(place_id):
         abort(404)
     return jsonify(place.to_dict())
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """Delete a Place object"""
     place = storage.get(Place, place_id)
@@ -31,7 +37,8 @@ def delete_place(place_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places',
+                 methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     """Create a Place object"""
     city = storage.get(City, city_id)
