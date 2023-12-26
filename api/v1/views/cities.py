dfaee09e6f505ddae461c5956496e2fd02d50fb3
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-26 09:26:08 +0100

    Implemented RESTful API endpoints for City objects in api/v1/views/cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module for City views"""
+"""Contains the cities view for the API.
+"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage, State, City
