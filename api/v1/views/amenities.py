2ccca1d2930b5ab466b655ba022a7bbb1d5705f7
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-26 12:06:45 +0100

    Implement RESTful API endpoints for Amenity model in v1/views

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,19 +2,22 @@
 """
 Module for Amenity views
 """
+from flask import abort, jsonify, request
 from api.v1.views import app_views
-from flask import jsonify, abort, request
-from models import storage, Amenity
+from models import storage
+from models.amenity import Amenity
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """Retrieve all amenities"""
-    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    amenities = [amenity.to_dict()
+                 for amenity in storage.all(Amenity).values()]
     return jsonify(amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieve amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
@@ -23,7 +26,8 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """Deletes an amenity"""
     amenity = storage.get(Amenity, amenity_id)
@@ -47,7 +51,8 @@ def create_amenity():
     return jsonify(amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """Updates an amenity"""
     amenity = storage.get(Amenity, amenity_id)
