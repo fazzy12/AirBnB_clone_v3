a29bed59d699749484b49d067549fccb0ae4e235
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-27 17:46:37 +0100

    Add API endpoints for managing the relationship between Place and Amenity objects

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """Place-Amenity view module"""
 
+from flask import abort, jsonify
 from api.v1.views import app_views
-from flask import jsonify, abort, request
 from models import storage
-from models.place import Place
 from models.amenity import Amenity
+from models.place import Place
 
 
 @app_views.route('/places/<place_id>/amenities',
                  methods=['GET'], strict_slashes=False)
 def get_amenities_by_place(place_id):
     """Retrieve all amenities of a place"""
-    place = storage.get("Place", place_id)
+    place = storage.get(Place, place_id)
     if not place:
         abort(404)
 
     if storage.__class__.__name__ == "DBStorage":
         amenities = [amenity.to_dict() for amenity in place.amenities]
     else:
-        amenities = [storage.get("Amenity", amenity_id).to_dict()
+        amenities = [storage.get(Amenity, amenity_id).to_dict()
                      for amenity_id in place.amenity_ids]
 
     return jsonify(amenities)
@@ -29,8 +29,8 @@ def get_amenities_by_place(place_id):
                  methods=['DELETE'], strict_slashes=False)
 def delete_amenity_from_place(place_id, amenity_id):
     """Delete an amenity from a place"""
-    place = storage.get("Place", place_id)
-    amenity = storage.get("Amenity", amenity_id)
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
 
     if not place or not amenity:
         abort(404)
@@ -52,8 +52,8 @@ def delete_amenity_from_place(place_id, amenity_id):
                  methods=['POST'], strict_slashes=False)
 def link_amenity_to_place(place_id, amenity_id):
     """Link an amenity to a place"""
-    place = storage.get("Place", place_id)
-    amenity = storage.get("Amenity", amenity_id)
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
 
     if not place or not amenity:
         abort(404)
