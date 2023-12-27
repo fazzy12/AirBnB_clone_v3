b1509523b6ce59dc8c0191978d0552effe3fec4e
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-27 17:14:39 +0100

    Set up CORS for API access

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
