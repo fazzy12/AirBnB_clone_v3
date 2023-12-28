d52a6a978e73100a2ec4c52c628bb6e1fa24ce2b
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-28 15:06:30 +0100

    Add search functionality for places based on criteria in places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''Contains the places view for the API.'''
+"""Places view module"""
+
 from flask import abort, jsonify, make_response, request
 import requests
 from api.v1.views import app_views
@@ -15,82 +16,74 @@ import json
 from os import getenv
 
 
-@app_views.route('cities/<city_id>/places',
-                 methods=['GET'], strict_slashes=False)
-def place(city_id):
-    """Retrieves the list of all Place objects of a City"""
-    obj_city = storage.get(City, city_id)
-    if not obj_city:
-        abort(404)
 
-    return jsonify([obj.to_dict() for obj in obj_city.places])
+@app_views.route('/cities/<city_id>/places',
+                 methods=['GET'], strict_slashes=False)
+def get_places_by_city(city_id):
+    """Retrieve all Place objects for a City"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify([place.to_dict() for place in city.places])
 
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
-def single_place(place_id):
-    """Retrieves a Place object"""
-    obj = storage.get(Place, place_id)
-    if not obj:
+def get_place(place_id):
+    """Retrieve a specific Place object"""
+    place = storage.get(Place, place_id)
+    if place is None:
         abort(404)
-    return jsonify(obj.to_dict())
+    return jsonify(place.to_dict())
 
 
 @app_views.route('/places/<place_id>',
                  methods=['DELETE'], strict_slashes=False)
-def del_place(place_id):
-    """Returns an empty dictionary with the status code 200"""
-    obj = storage.get(Place, place_id)
-    if not obj:
+def delete_place(place_id):
+    """Delete a Place object"""
+    place = storage.get(Place, place_id)
+    if place is None:
         abort(404)
-    obj.delete()
+    place.delete()
     storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
 
 
-@app_views.route('cities/<city_id>/places',
+@app_views.route('/cities/<city_id>/places',
                  methods=['POST'], strict_slashes=False)
-def post_place(city_id):
-    """Returns the new Place with the status code 201"""
-    obj_city = storage.get(City, city_id)
-    if not obj_city:
+def create_place(city_id):
+    """Create a Place object"""
+    city = storage.get(City, city_id)
+    if city is None:
         abort(404)
-
-    new_place = request.get_json()
-    if not new_place:
-        abort(400, 'Not a JSON')
-    if 'user_id' not in new_place:
+    req_json = request.get_json()
+    if not req_json:
+        abort(400, "Not a JSON")
+    if "user_id" not in req_json:
         abort(400, "Missing user_id")
-    user_id = new_place['user_id']
-    obj_user = storage.get(User, user_id)
-    if not obj_user:
+    if storage.get(User, req_json["user_id"]) is None:
         abort(404)
-    if 'name' not in new_place:
+    if "name" not in req_json:
         abort(400, "Missing name")
-
-    obj = Place(**new_place)
-    setattr(obj, 'city_id', city_id)
-    storage.new(obj)
-    storage.save()
-    return make_response(jsonify(obj.to_dict()), 201)
+    new_place = Place(**req_json)
+    new_place.city_id = city_id
+    new_place.save()
+    return jsonify(new_place.to_dict()), 201
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-def put_place(place_id):
-    """Returns the Place object with the status code 200"""
-    obj = storage.get(Place, place_id)
-    if not obj:
+def update_place(place_id):
+    """Update a Place object"""
+    place = storage.get(Place, place_id)
+    if place is None:
         abort(404)
-
-    req = request.get_json()
-    if not req:
+    req_json = request.get_json()
+    if not req_json:
         abort(400, "Not a JSON")
-
-    for k, v in req.items():
-        if k not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
-            setattr(obj, k, v)
-
-    storage.save()
-    return make_response(jsonify(obj.to_dict()), 200)
+    for key, value in req_json.items():
+        if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
+            setattr(place, key, value)
+    place.save()
+    return jsonify(place.to_dict()), 200
 
 
 @app_views.route('/places_search', methods=['POST'], strict_slashes=False)
