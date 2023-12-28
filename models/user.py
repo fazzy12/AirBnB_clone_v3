4b938f4760e5116bc55937b7d4009bb64446aebf
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-28 13:38:42 +0100

    Implement security improvements for User passwords

--- models/user.py
+++ models/user.py
@@ -3,8 +3,6 @@
 import hashlib
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
